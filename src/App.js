308f6afb8018159aacb501477441c3684c8378d0
Anevo <cezar.vulcu@gmail.com>
2018-11-08 15:55:26 +0000

    Update: Added Form Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Titles from "./Components/Titles";
+import Form from "./Components/Form";
 
 class App extends React.PureComponent {
   render() {
     return (
       <div>
-        <Titles/>
+        <Titles />
+        <Form />
       </div>
     );
   }
---
+++ src/Components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Form extends React.PureComponent {
+    render() {
+        return (
+            <div>Form Component</div>
+        );
+    }
+}
+
+export default Form;
